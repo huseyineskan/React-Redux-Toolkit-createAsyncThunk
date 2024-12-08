b40cf6570a840cf00f2f0d346abe19f9e425f148
Huseyin Eskan <huseyineskan01@gmail.com>
2024-12-08 01:10:31 +0100

    added a table

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -4,9 +4,9 @@ import { getAllUsers } from "../redux/userSlice";
 
 function UserList() {
   const dispatch = useDispatch();
+
   const { users } = useSelector((store) => store.user);
   console.log(users);
-
   useEffect(() => {
     dispatch(getAllUsers());
   }, []);
@@ -14,9 +14,29 @@ function UserList() {
   return (
     <div>
       <h3>ALL USERS</h3>
-      <ul>
-        {users && users.map((user) => <li key={user.id}>{user.name}</li>)}
-      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Website</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users &&
+            users.map((user) => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                <td>{user.phone}</td>
+                <td>{user.website}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </div>
   );
 }
